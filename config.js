e416d99097131ab2387135801dcecf1708850211
Malik,Salil <salilmalik92@gmail.com>
2016-12-25 22:55:53 +0530

    Changed port number and database

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-'port': process.env.PORT || 8080,
-'database': 'mongodb://donupapp:kuchbhi77@ds039860.mlab.com:39860/donup',
-'secret': 'kuchbhi'
+    'port': process.env.PORT || 80,
+    'DATABASE': 'mongodb://donupapp:qwerty123@ds145118.mlab.com:45118/donup',
+    'secret': 'kuchbhi'
 };
