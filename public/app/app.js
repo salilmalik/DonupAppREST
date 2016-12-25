9b736b2ce19d936457071ab5b3ecea0a1a803805
Malik,Salil <salilmalik92@gmail.com>
2016-12-26 00:04:36 +0530

    Changed facebook id for login

--- public/app/app.js
+++ public/app/app.js
@@ -2,7 +2,7 @@
 	'use strict';
 	var app = angular.module('donup', ['ngRoute', 'ngCookies', 'ngFileUpload', 'satellizer']).config(function ($authProvider) {
 		$authProvider.facebook({
-			clientId: '1228445853873951',
+			clientId: '1073914582717265',
 			url: '/api/user/fb'
 		});
 	});
