356d98074376d8e3db653a4340f89821efa62c57
Malik,Salil <salilmalik92@gmail.com>
2017-01-02 21:06:15 +0530

    Test for watermark

--- public/app/controllers/uploadCtrl.js
+++ public/app/controllers/uploadCtrl.js
@@ -1,15 +1,14 @@
-(function() {
+(function () {
 	'use strict';
 
 	var app = angular.module('donup');
-	app.controller('UploadCtrl', [ '$scope','$rootScope','$cookies', controller ]);
+	app.controller('UploadCtrl', ['$scope', '$rootScope', '$cookies', controller]);
 	function controller($scope, $rootScope, $cookies) {
-		$rootScope.loggedInUserToken=$cookies.get('usertoken');
-		$rootScope.loggedInUsername=$cookies.get('username');
-		if($cookies.get('userId')===undefined){
-		$rootScope.loggedInUsername = $cookies.put('username', undefined);
-		$rootScope.loggedInUserToken = $cookies.put('usertoken', undefined);
+		$rootScope.loggedInUserToken = $cookies.get('usertoken');
+		$rootScope.loggedInUsername = $cookies.get('username');
+		if ($cookies.get('userId') === undefined) {
+			$rootScope.loggedInUsername = $cookies.put('username', undefined);
+			$rootScope.loggedInUserToken = $cookies.put('usertoken', undefined);
+		}
 	}
-	}
-
 })();
--- public/app/directives/imageDirective.js
+++ public/app/directives/imageDirective.js
@@ -5,60 +5,59 @@
             restrict: 'A',
             templateUrl: 'app/directives/imageUpload.html',
             scope: {
-                files: '='
+                files: '=',
+                watermarkText: '='
             },
             controller: ['$scope', 'Upload', '$timeout', 'ImageService', '$cookies', '$location', function ($scope, Upload, $timeout, imageService, $cookies, $location) {
-               
+
                 $scope.images = {};
                 $scope.userId = $cookies.get('userId');
-                console.log('IMAGE UPLAOD '+$scope.userId);
-				$scope.imagesToUpload=[];
+                console.log('IMAGE UPLAOD ' + $scope.userId);
+                $scope.imagesToUpload = [];
                 $scope.$watch('files', function () {
                     $scope.upload($scope.files);
                 });
-                  $scope.upload = function (files) {
+                $scope.upload = function (files) {
                     if (files && files.length) {
                         for (var i = 0; i < files.length; i++) {
                             var file = files[i];
                             Upload.upload({
                                 url: 'api/image',
-                                fields: { 'userId': $scope.userId },
+                                fields: { 'userId': $scope.userId, 'watermarkText': $scope.watermarkText },
                                 file: file
                             }).progress(function (evt) {
                                 var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
-                                $scope.progress=progressPercentage;
+                                $scope.progress = progressPercentage;
                             }).success(function (data, status, headers, config) {
-								$scope.imagesToUpload.push(data.imageId);
-								console.log(JSON.stringify(data));
-								if(i==1){
-                                    console.log('i'+i+JSON.stringify(data.imageId));
-                                $location.path("/imageLinks/"+data.imageId);
-								}else{
-                                      console.log('i'+i+JSON.stringify(data.imageId));
-                                $rootScope.imagesToUpload=$scope.imagesToUpload;
-								$location.path("/multiImage");
-								}
-                           }).error(function (data, status, headers, config) {
+                                $scope.imagesToUpload.push(data.imageId);
+                                console.log(JSON.stringify(data));
+                                if (i == 1) {
+                                    $location.path("/imageLinks/" + data.imageId);
+                                } else {
+                                    $rootScope.imagesToUpload = $scope.imagesToUpload;
+                                    $location.path("/multiImage");
+                                }
+                            }).error(function (data, status, headers, config) {
                                 console.log('error status: ' + status);
                             })
                         }
                     }
                 };
-         
-            } ]
+
+            }]
         };
     });
-app.directive('selectOnClick', ['$window', function ($window) {
-    return {
-        restrict: 'A',
-        link: function (scope, element, attrs) {
-            element.on('click', function () {
-                if (!$window.getSelection().toString()) {
-                    // Required for mobile Safari
-                    this.setSelectionRange(0, this.value.length)
-                }
-            });
-        }
-    };
-}]);
+    app.directive('selectOnClick', ['$window', function ($window) {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.on('click', function () {
+                    if (!$window.getSelection().toString()) {
+                        // Required for mobile Safari
+                        this.setSelectionRange(0, this.value.length)
+                    }
+                });
+            }
+        };
+    }]);
 })();
