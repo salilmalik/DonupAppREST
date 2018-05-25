8ae6997d2bc470661430db0427e79a61d88ab8d9
salilmalik <salilmalik92@gmail.com>
2018-05-24 22:51:10 -0700

    configured port

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    'PORT': process.env.PORT || 80,
+    'PORT': process.env.PORT || 3000,
     'DATABASE': 'mongodb://donup:kuchbhi@ds039860.mlab.com:39860/donup',
     'SECRET': 'kuchbhi',
     'FB_CLIENT_SECRET': 'dfd8e0d6d38bad445ab84313dc8c7c18',
